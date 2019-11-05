169eb8f66fedb47c7e5143502d301c33046b56b1
Dainis19 <48949357+Dainis19@users.noreply.github.com>
2019-11-05 10:26:34 +0200

    Update led.py

--- led.py
+++ led.py
@@ -9,7 +9,7 @@ try:
     while counter < 30:
         print ('LED 1 on')
         GPIO.output(18,GPIO.HIGH)
-        time.sleep(1)
+        time.sleep(3)
         print ('Led off')
         GPIO.output(18,GPIO.LOW)
         print ('kaut kas jauns')
