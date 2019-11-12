4c05ac1511a8435684f8d7ed9e99d8cd8bca032b
Dainis19 <48949357+Dainis19@users.noreply.github.com>
2019-11-12 08:43:31 +0200

    Update poga.py

--- poga.py
+++ poga.py
@@ -1,1 +1,16 @@
+import time
+import RPI.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
+button = 4
+
+GPIO.setup(button, GPIO.IN, GPIO.PUD.UP)
+
+while True:
+    button_state = GPIO.input(button)
+    if button_state == GPIO.HIGH:
+        print ("HIGH")
+    else:
+        print ("LOW")
+    time.sleep(0.5)
